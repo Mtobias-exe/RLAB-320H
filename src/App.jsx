2f2129c340bc42874ff9409357453d261a9ad5fd
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-21 12:14:00 -0500

    added components to app

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,79 @@ import SubmitBtn from '../components/submitBtn';
 
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, dispatch] = useReducer(TodoReducer, initialState);
+  
+  const todoList = todos.map((task) => {
+    return (
+     <Todo key={task.id} title={task.title} dispatch={dispatch}/>
+    );
+  });
+  
 
   return (
     <>
+      <h1>Todo List</h1>
+     <div>
+     <TextInput state={input} setState={setInput}/>
+     <SubmitBtn
+     type="submit-todo"
+     payload={{title: input}}
+     dispatch={dispatch}
      
+     >
+      +
+     </SubmitBtn>
+     </div>
+     <div>
+      <h2>Todos</h2>
+      {todoList}
+     </div>
+
     </>
   )
 }
 
+const initialState = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  }
+
+
+];
 
 
 
