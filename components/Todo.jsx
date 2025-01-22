8e5bcdc34e017e78aa36a4351a1f14371b2d3d14
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-21 22:16:39 -0500

    added submit-todo

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 function Todo({ title, dispatch, id }) {
   return (
     <div>
       <span>{title}</span>
-      <button onClick={() => dispatch({ type: 'toggle-todo', payload: { id } })}>Toggle</button>
+      <button onClick={() => dispatch({ type: 'edit-todo', payload: { id } })}>Edit</button>
       <button onClick={() => dispatch({ type: 'remove-todo', payload: { id } })}>Remove</button>
     </div>
   );
--- components/TodoReducer.jsx
+++ components/TodoReducer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
-function todoReducer() {
-  return (
-    <div>todoReducer</div>
-  )
+function TodoReducer(state,{type, payload: {title}} ) {
+  switch (type) {
+    case "submit-todo": {
+      if (title === ""){
+       return state;
+      }
+      let hasThisTodo = false;
+    state.forEach((e) =>{
+      if (e.title === title){
+        alert(`There is already a ${title} in your todo`);
+        hasThisTodo = true;
+      }
+    })
+    if (hasThisTodo) return state;
+    return [{title: title}, ...state];
+    }
+    case "remove-todo":
+      return state.filter((s) => s.title !== title);
+   
+      default:
+        throw Error("Unknown Action: " + type)
+  }
+
 }
 
-export default todoReducer
+
+export default TodoReducer
